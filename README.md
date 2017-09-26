472038edf0c2dd00ad333526487609ebd11b0a20
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2017-09-27 09:47:37 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BokkyPooBahsAssistedTokenTransfer
+BokkyPooBahsAssistedTokenTransfer
---
+++ contracts/BATTToken.sol
@@ -0,0 +1,230 @@
+pragma solidity ^0.4.16;
+
+// ----------------------------------------------------------------------------
+// BATTT 'BokkyPooBah's Assisted Token Transfer Token' token contract
+//
+// Potential buyers of this token are encouraged to perform their due diligence
+// on the business proposition and individuals behind this token
+//
+// Deployed to : 
+// Symbol      : BATTT
+// Name        : BokkyPooBah's Assisted Token Transfer Token
+// Total supply: 10 million
+// Decimals    : 18
+//
+// Enjoy. (c) BokkyPooBah / Bok Consulting Pty Ltd 2017. The MIT Licence.
+// ----------------------------------------------------------------------------
+
+
+// ----------------------------------------------------------------------------
+// ERC Token Standard #20 Interface
+// https://github.com/ethereum/EIPs/issues/20
+// ----------------------------------------------------------------------------
+contract ERC20Interface {
+    uint public totalSupply;
+    function balanceOf(address _owner) public constant returns (uint balance);
+    function transfer(address _to, uint _value) public returns (bool success);
+    function transferFrom(address _from, address _to, uint _value)
+        public returns (bool success);
+    function approve(address _spender, uint _value)
+        public returns (bool success);
+    function allowance(address _owner, address _spender)
+        public constant returns (uint remaining);
+    event Transfer(address indexed _from, address indexed _to, uint _value);
+    event Approval(address indexed _owner, address indexed _spender,
+        uint _value);
+}
+
+
+// ----------------------------------------------------------------------------
+// Owned contract
+// ----------------------------------------------------------------------------
+contract Owned {
+
+    // ------------------------------------------------------------------------
+    // Current owner, and proposed new owner
+    // ------------------------------------------------------------------------
+    address public owner;
+    address public newOwner;
+
+    // ------------------------------------------------------------------------
+    // Constructor - assign creator as the owner
+    // ------------------------------------------------------------------------
+    function Owned() public {
+        owner = msg.sender;
+    }
+
+
+    // ------------------------------------------------------------------------
+    // Modifier to mark that a function can only be executed by the owner
+    // ------------------------------------------------------------------------
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+
+    // ------------------------------------------------------------------------
+    // Owner can initiate transfer of contract to a new owner
+    // ------------------------------------------------------------------------
+    function transferOwnership(address _newOwner) public onlyOwner {
+        newOwner = _newOwner;
+    }
+
+
+    // ------------------------------------------------------------------------
+    // New owner has to accept transfer of contract
+    // ------------------------------------------------------------------------
+    function acceptOwnership() public {
+        require(msg.sender == newOwner);
+        OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+        newOwner = 0x0;
+    }
+    event OwnershipTransferred(address indexed _from, address indexed _to);
+}
+
+
+// ----------------------------------------------------------------------------
+// Safe maths, borrowed from OpenZeppelin
+// ----------------------------------------------------------------------------
+library SafeMath {
+
+    // ------------------------------------------------------------------------
+    // Add a number to another number, checking for overflows
+    // ------------------------------------------------------------------------
+    function add(uint a, uint b) public pure returns (uint) {
+        uint c = a + b;
+        assert(c >= a && c >= b);
+        return c;
+    }
+
+    // ------------------------------------------------------------------------
+    // Subtract a number from another number, checking for underflows
+    // ------------------------------------------------------------------------
+    function sub(uint a, uint b) public pure returns (uint) {
+        assert(b <= a);
+        return a - b;
+    }
+}
+
+
+// ----------------------------------------------------------------------------
+// ERC20 Token, with the addition of symbol, name and decimals
+// ----------------------------------------------------------------------------
+contract BATTToken is ERC20Interface, Owned {
+    using SafeMath for uint;
+
+    // ------------------------------------------------------------------------
+    // Token parameters
+    // ------------------------------------------------------------------------
+    string public constant symbol = "BATTT";
+    string public constant name = "BokkyPooBah's Assisted Token Transfer Token";
+    uint8 public constant decimals = 18;
+
+    uint public constant totalSupply = 10 * 10**6 * 10**18;
+
+    // ------------------------------------------------------------------------
+    // Balances for each account
+    // ------------------------------------------------------------------------
+    mapping(address => uint) balances;
+
+    // ------------------------------------------------------------------------
+    // Owner of account approves the transfer of an amount to another account
+    // ------------------------------------------------------------------------
+    mapping(address => mapping (address => uint)) allowed;
+
+
+    // ------------------------------------------------------------------------
+    // Constructor
+    // ------------------------------------------------------------------------
+    function BATTToken() public {
+        balances[owner] = totalSupply;
+    }
+
+
+    // ------------------------------------------------------------------------
+    // Get the account balance of another account with address _owner
+    // ------------------------------------------------------------------------
+    function balanceOf(address _owner) public constant returns (uint balance) {
+        return balances[_owner];
+    }
+
+
+    // ------------------------------------------------------------------------
+    // Transfer the balance from owner's account to another account
+    // - Account must have sufficient balance to transfer
+    // - 0 value transfers are allowed
+    // ------------------------------------------------------------------------
+    function transfer(address _to, uint _amount) public returns (bool success) {
+        balances[msg.sender] = balances[msg.sender].sub(_amount);
+        balances[_to] = balances[_to].add(_amount);
+        Transfer(msg.sender, _to, _amount);
+        return true;
+    }
+
+
+    // ------------------------------------------------------------------------
+    // Allow _spender to withdraw from your account, multiple times, up to the
+    // _value amount. If this function is called again it overwrites the
+    // current allowance with _value.
+    // ------------------------------------------------------------------------
+    function approve(
+        address _spender,
+        uint _amount
+    ) public returns (bool success) {
+        allowed[msg.sender][_spender] = _amount;
+        Approval(msg.sender, _spender, _amount);
+        return true;
+    }
+
+
+    // ------------------------------------------------------------------------
+    // Spender of tokens transfer an amount of tokens from the token owner's
+    // balance to another account. The owner of the tokens must already
+    // have approve(...)-d this transfer
+    // - From account must have sufficient balance to transfer
+    // - Spender must have sufficient allowance to transfer
+    // - 0 value transfers are allowed
+    // ------------------------------------------------------------------------
+    function transferFrom(
+        address _from,
+        address _to,
+        uint _amount
+    ) public returns (bool success) {
+        balances[_from] = balances[_from].sub(_amount);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_amount);
+        balances[_to] = balances[_to].add(_amount);
+        Transfer(_from, _to, _amount);
+        return true;
+    }
+
+
+    // ------------------------------------------------------------------------
+    // Returns the amount of tokens approved by the owner that can be
+    // transferred to the spender's account
+    // ------------------------------------------------------------------------
+    function allowance(
+        address _owner,
+        address _spender
+    ) public constant returns (uint remaining) {
+        return allowed[_owner][_spender];
+    }
+
+
+    // ------------------------------------------------------------------------
+    // Don't accept ethers - no payable modifier
+    // ------------------------------------------------------------------------
+    function () public {
+    }
+
+
+    // ------------------------------------------------------------------------
+    // Owner can transfer out any accidentally sent ERC20 tokens
+    // ------------------------------------------------------------------------
+    function transferAnyERC20Token(address tokenAddress, uint amount)
+      public onlyOwner returns (bool success)
+    {
+        return ERC20Interface(tokenAddress).transfer(owner, amount);
+    }
+}
