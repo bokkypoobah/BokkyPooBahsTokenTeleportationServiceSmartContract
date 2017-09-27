b94a831889a9e3960b05066bcfd07078dfe2f430
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2017-09-27 13:59:26 +1000

    Testing

---
+++ test/01_test1.sh
@@ -0,0 +1,254 @@
+#!/bin/bash
+# ----------------------------------------------------------------------------------------------
+# Testing the smart contract
+#
+# Enjoy. (c) BokkyPooBah / Bok Consulting Pty Ltd 2017. The MIT Licence.
+# ----------------------------------------------------------------------------------------------
+
+MODE=${1:-test}
+
+GETHATTACHPOINT=`grep ^IPCFILE= settings.txt | sed "s/^.*=//"`
+PASSWORD=`grep ^PASSWORD= settings.txt | sed "s/^.*=//"`
+
+SOURCEDIR=`grep ^SOURCEDIR= settings.txt | sed "s/^.*=//"`
+
+CROWDSALESOL=`grep ^CROWDSALESOL= settings.txt | sed "s/^.*=//"`
+CROWDSALEJS=`grep ^CROWDSALEJS= settings.txt | sed "s/^.*=//"`
+
+DEPLOYMENTDATA=`grep ^DEPLOYMENTDATA= settings.txt | sed "s/^.*=//"`
+
+INCLUDEJS=`grep ^INCLUDEJS= settings.txt | sed "s/^.*=//"`
+TEST1OUTPUT=`grep ^TEST1OUTPUT= settings.txt | sed "s/^.*=//"`
+TEST1RESULTS=`grep ^TEST1RESULTS= settings.txt | sed "s/^.*=//"`
+
+CURRENTTIME=`date +%s`
+CURRENTTIMES=`date -r $CURRENTTIME -u`
+
+BLOCKSINDAY=10
+
+if [ "$MODE" == "dev" ]; then
+  # Start time now
+  STARTTIME=`echo "$CURRENTTIME" | bc`
+else
+  # Start time 1m 10s in the future
+  STARTTIME=`echo "$CURRENTTIME+90" | bc`
+fi
+STARTTIME_S=`date -r $STARTTIME -u`
+ENDTIME=`echo "$CURRENTTIME+60*3" | bc`
+ENDTIME_S=`date -r $ENDTIME -u`
+
+printf "MODE            = '$MODE'\n" | tee $TEST1OUTPUT
+printf "GETHATTACHPOINT = '$GETHATTACHPOINT'\n" | tee -a $TEST1OUTPUT
+printf "PASSWORD        = '$PASSWORD'\n" | tee -a $TEST1OUTPUT
+printf "SOURCEDIR       = '$SOURCEDIR'\n" | tee -a $TEST1OUTPUT
+printf "CROWDSALESOL    = '$CROWDSALESOL'\n" | tee -a $TEST1OUTPUT
+printf "CROWDSALEJS     = '$CROWDSALEJS'\n" | tee -a $TEST1OUTPUT
+printf "DEPLOYMENTDATA  = '$DEPLOYMENTDATA'\n" | tee -a $TEST1OUTPUT
+printf "INCLUDEJS       = '$INCLUDEJS'\n" | tee -a $TEST1OUTPUT
+printf "TEST1OUTPUT     = '$TEST1OUTPUT'\n" | tee -a $TEST1OUTPUT
+printf "TEST1RESULTS    = '$TEST1RESULTS'\n" | tee -a $TEST1OUTPUT
+printf "CURRENTTIME     = '$CURRENTTIME' '$CURRENTTIMES'\n" | tee -a $TEST1OUTPUT
+printf "STARTTIME       = '$STARTTIME' '$STARTTIME_S'\n" | tee -a $TEST1OUTPUT
+printf "ENDTIME         = '$ENDTIME' '$ENDTIME_S'\n" | tee -a $TEST1OUTPUT
+
+# Make copy of SOL file and modify start and end times ---
+# `cp modifiedContracts/SnipCoin.sol .`
+`cp $SOURCEDIR/$CROWDSALESOL .`
+
+# --- Modify parameters ---
+# `perl -pi -e "s/bool transferable/bool public transferable/" $TOKENSOL`
+# `perl -pi -e "s/MULTISIG_WALLET_ADDRESS \= 0xc79ab28c5c03f1e7fbef056167364e6782f9ff4f;/MULTISIG_WALLET_ADDRESS \= 0xa22AB8A9D641CE77e06D98b7D7065d324D3d6976;/" GimliCrowdsale.sol`
+# `perl -pi -e "s/START_DATE = 1505736000;.*$/START_DATE \= $STARTTIME; \/\/ $STARTTIME_S/" GimliCrowdsale.sol`
+# `perl -pi -e "s/END_DATE = 1508500800;.*$/END_DATE \= $ENDTIME; \/\/ $ENDTIME_S/" GimliCrowdsale.sol`
+# `perl -pi -e "s/VESTING_1_DATE = 1537272000;.*$/VESTING_1_DATE \= $VESTING1TIME; \/\/ $VESTING1TIME_S/" GimliCrowdsale.sol`
+# `perl -pi -e "s/VESTING_2_DATE = 1568808000;.*$/VESTING_2_DATE \= $VESTING2TIME; \/\/ $VESTING2TIME_S/" GimliCrowdsale.sol`
+
+DIFFS1=`diff $SOURCEDIR/$CROWDSALESOL $CROWDSALESOL`
+echo "--- Differences $SOURCEDIR/$CROWDSALESOL $CROWDSALESOL ---" | tee -a $TEST1OUTPUT
+echo "$DIFFS1" | tee -a $TEST1OUTPUT
+
+echo "var tokenOutput=`solc --optimize --combined-json abi,bin,interface $CROWDSALESOL`;" > $CROWDSALEJS
+
+geth --verbosity 3 attach $GETHATTACHPOINT << EOF | tee -a $TEST1OUTPUT
+loadScript("$CROWDSALEJS");
+loadScript("functions.js");
+
+var libAbi = JSON.parse(tokenOutput.contracts["$CROWDSALESOL:SafeMath"].abi);
+var libBin = "0x" + tokenOutput.contracts["$CROWDSALESOL:SafeMath"].bin;
+var tokenAbi = JSON.parse(tokenOutput.contracts["$CROWDSALESOL:BATTToken"].abi);
+var tokenBin = "0x" + tokenOutput.contracts["$CROWDSALESOL:BATTToken"].bin;
+
+// console.log("DATA: libAbi=" + JSON.stringify(libAbi));
+// console.log("DATA: libBin=" + JSON.stringify(libBin));
+// console.log("DATA: tokenAbi=" + JSON.stringify(tokenAbi));
+// console.log("DATA: tokenBin=" + JSON.stringify(tokenBin));
+
+unlockAccounts("$PASSWORD");
+printBalances();
+console.log("RESULT: ");
+
+
+// -----------------------------------------------------------------------------
+var deployLibraryMessage = "Deploy Crowdsale/Token Contract";
+// -----------------------------------------------------------------------------
+console.log("RESULT: " + deployLibraryMessage);
+var libContract = web3.eth.contract(libAbi);
+// console.log(JSON.stringify(tokenContract));
+var libTx = null;
+var libAddress = null;
+
+var lib = libContract.new({from: contractOwnerAccount, data: libBin, gas: 6000000},
+  function(e, contract) {
+    if (!e) {
+      if (!contract.address) {
+        libTx = contract.transactionHash;
+      } else {
+        libAddress = contract.address;
+        addAccount(libAddress, "Lib SafeMath");
+        console.log("DATA: libAddress=" + libAddress);
+      }
+    }
+  }
+);
+
+while (txpool.status.pending > 0) {
+}
+
+printTxData("libAddress=" + libAddress, libTx);
+printBalances();
+failIfGasEqualsGasUsed(libTx, deployLibraryMessage);
+printTokenContractDetails();
+console.log("RESULT: ");
+
+
+// -----------------------------------------------------------------------------
+var tokenMessage = "Deploy Crowdsale/Token Contract";
+console.log("RESULT: old='" + tokenBin + "'");
+var newTokenBin = tokenBin.replace(/__BATTToken\.sol\:SafeMath________________/g, libAddress.substring(2, 42));
+console.log("RESULT: new='" + newTokenBin + "'");
+// -----------------------------------------------------------------------------
+console.log("RESULT: " + tokenMessage);
+var tokenContract = web3.eth.contract(tokenAbi);
+// console.log(JSON.stringify(tokenContract));
+var tokenTx = null;
+var tokenAddress = null;
+
+var token = tokenContract.new({from: contractOwnerAccount, data: newTokenBin, gas: 6000000},
+  function(e, contract) {
+    if (!e) {
+      if (!contract.address) {
+        tokenTx = contract.transactionHash;
+      } else {
+        tokenAddress = contract.address;
+        addAccount(tokenAddress, "Token '" + token.symbol() + "' '" + token.name() + "'");
+        addTokenContractAddressAndAbi(tokenAddress, tokenAbi);
+        console.log("DATA: tokenAddress=" + tokenAddress);
+      }
+    }
+  }
+);
+
+while (txpool.status.pending > 0) {
+}
+
+printTxData("tokenAddress=" + tokenAddress, tokenTx);
+printBalances();
+failIfGasEqualsGasUsed(tokenTx, tokenMessage);
+printTokenContractDetails();
+console.log("RESULT: ");
+
+
+// -----------------------------------------------------------------------------
+var transferTokenMessage = "Transfer Tokens";
+// -----------------------------------------------------------------------------
+console.log("RESULT: " + transferTokenMessage);
+var transferToken1Tx = token.transfer(account3, "100000000000000000000", {from: contractOwnerAccount, gas: 100000});
+var transferToken2Tx = token.transfer(account4, "100000000000000000000", {from: contractOwnerAccount, gas: 100000});
+while (txpool.status.pending > 0) {
+}
+printTxData("transferToken1Tx", transferToken1Tx);
+printTxData("transferToken2Tx", transferToken2Tx);
+printBalances();
+failIfGasEqualsGasUsed(transferToken1Tx, transferTokenMessage + " - transfer 10,000 tokens ac1 -> ac3");
+failIfGasEqualsGasUsed(transferToken2Tx, transferTokenMessage + " - transfer 10,000 tokens ac1 -> ac4");
+printTokenContractDetails();
+console.log("RESULT: ");
+
+
+// -----------------------------------------------------------------------------
+var moveTokenMessage = "Move 0 Tokens After Transfers Allowed";
+// -----------------------------------------------------------------------------
+console.log("RESULT: " + moveTokenMessage);
+var moveToken1Tx = token.transfer(account5, "0", {from: account3, gas: 100000});
+var moveToken3Tx = token.transferFrom(account4, account7, "0", {from: account6, gas: 100000});
+while (txpool.status.pending > 0) {
+}
+printTxData("moveToken1Tx", moveToken1Tx);
+printTxData("moveToken3Tx", moveToken3Tx);
+printBalances();
+failIfGasEqualsGasUsed(moveToken1Tx, moveTokenMessage + " - transfer 0 tokens ac3 -> ac5. SHOULD not throw");
+failIfGasEqualsGasUsed(moveToken3Tx, moveTokenMessage + " - transferFrom 0 tokens ac4 -> ac7 by ac6. SHOULD not throw");
+printTokenContractDetails();
+console.log("RESULT: ");
+
+
+// -----------------------------------------------------------------------------
+var moveTokenMessage = "Move More Tokens Than Owned";
+// -----------------------------------------------------------------------------
+console.log("RESULT: " + moveTokenMessage);
+var moveToken1Tx = token.transfer(account5, "3000000000000000000000001", {from: account3, gas: 100000});
+var moveToken2Tx = token.approve(account6,  "3000000000000000000000001", {from: account4, gas: 100000});
+while (txpool.status.pending > 0) {
+}
+var moveToken3Tx = token.transferFrom(account4, account7, "3000000000000000000000001", {from: account6, gas: 100000});
+while (txpool.status.pending > 0) {
+}
+printTxData("moveToken1Tx", moveToken1Tx);
+printTxData("moveToken2Tx", moveToken2Tx);
+printTxData("moveToken3Tx", moveToken3Tx);
+printBalances();
+passIfGasEqualsGasUsed(moveToken1Tx, moveTokenMessage + " - transfer 300K+1e-18 tokens ac3 -> ac5. SHOULD throw");
+failIfGasEqualsGasUsed(moveToken2Tx, moveTokenMessage + " - approve 300K+1e-18 tokens ac4 -> ac6");
+passIfGasEqualsGasUsed(moveToken3Tx, moveTokenMessage + " - transferFrom 300K+1e-18 tokens ac4 -> ac7 by ac6. SHOULD throw");
+printTokenContractDetails();
+console.log("RESULT: ");
+
+
+// -----------------------------------------------------------------------------
+var approveTokenMessage = "Change Approval Without Setting To 0";
+// -----------------------------------------------------------------------------
+console.log("RESULT: " + approveTokenMessage);
+var approveToken2Tx = token.approve(account6,  "3000000000000000000000002", {from: account4, gas: 100000});
+while (txpool.status.pending > 0) {
+}
+printTxData("approveToken2Tx", approveToken2Tx);
+printBalances();
+passIfGasEqualsGasUsed(approveToken2Tx, approveTokenMessage + " - approve 300K+2e-18 tokens ac4 -> ac6. SHOULD throw");
+printTokenContractDetails();
+console.log("RESULT: ");
+
+
+// -----------------------------------------------------------------------------
+var approveTokenMessage = "Change Approval By Setting To 0 In Between";
+// -----------------------------------------------------------------------------
+console.log("RESULT: " + approveTokenMessage);
+var approveToken2Tx = token.approve(account6,  "0", {from: account4, gas: 100000});
+while (txpool.status.pending > 0) {
+}
+var approveToken3Tx = token.approve(account6,  "3000000000000000000000002", {from: account4, gas: 100000});
+while (txpool.status.pending > 0) {
+}
+printTxData("approveToken2Tx", approveToken2Tx);
+printTxData("approveToken3Tx", approveToken3Tx);
+printBalances();
+failIfGasEqualsGasUsed(approveToken2Tx, approveTokenMessage + " - approve 0 tokens ac4 -> ac6");
+failIfGasEqualsGasUsed(approveToken3Tx, approveTokenMessage + " - approve 300K+2e-18 tokens ac4 -> ac6");
+printTokenContractDetails();
+console.log("RESULT: ");
+
+
+EOF
+grep "DATA: " $TEST1OUTPUT | sed "s/DATA: //" > $DEPLOYMENTDATA
+cat $DEPLOYMENTDATA
+grep "RESULT: " $TEST1OUTPUT | sed "s/RESULT: //" > $TEST1RESULTS
+cat $TEST1RESULTS
---
+++ test/functions.js
@@ -0,0 +1,234 @@
+// Jun 12 2017
+var ethPriceUSD = 380.39;
+
+// -----------------------------------------------------------------------------
+// Accounts
+// -----------------------------------------------------------------------------
+var accounts = [];
+var accountNames = {};
+
+addAccount(eth.accounts[0], "Account #0 - Miner");
+addAccount(eth.accounts[1], "Account #1 - Contract Owner");
+addAccount(eth.accounts[2], "Account #2 - Wallet");
+addAccount(eth.accounts[3], "Account #3 - Uncapped");
+addAccount(eth.accounts[4], "Account #4 - Capped");
+addAccount(eth.accounts[5], "Account #5");
+addAccount(eth.accounts[6], "Account #6");
+addAccount(eth.accounts[7], "Account #7");
+addAccount(eth.accounts[8], "Account #8");
+addAccount(eth.accounts[9], "Account #9");
+addAccount(eth.accounts[10], "Account #10");
+
+
+var minerAccount = eth.accounts[0];
+var contractOwnerAccount = eth.accounts[1];
+var wallet = eth.accounts[2];
+var account3 = eth.accounts[3];
+var account4 = eth.accounts[4];
+var account5 = eth.accounts[5];
+var account6 = eth.accounts[6];
+var account7 = eth.accounts[7];
+var account8 = eth.accounts[8];
+var account9 = eth.accounts[9];
+var account10 = eth.accounts[10];
+
+var baseBlock = eth.blockNumber;
+
+function unlockAccounts(password) {
+  for (var i = 0; i < eth.accounts.length; i++) {
+    personal.unlockAccount(eth.accounts[i], password, 100000);
+  }
+}
+
+function addAccount(account, accountName) {
+  accounts.push(account);
+  accountNames[account] = accountName;
+}
+
+
+// -----------------------------------------------------------------------------
+// Token Contract
+// -----------------------------------------------------------------------------
+var tokenContractAddress = null;
+var tokenContractAbi = null;
+
+function addTokenContractAddressAndAbi(address, tokenAbi) {
+  tokenContractAddress = address;
+  tokenContractAbi = tokenAbi;
+}
+
+
+// -----------------------------------------------------------------------------
+// Account ETH and token balances
+// -----------------------------------------------------------------------------
+function printBalances() {
+  var token = tokenContractAddress == null || tokenContractAbi == null ? null : web3.eth.contract(tokenContractAbi).at(tokenContractAddress);
+  var decimals = token == null ? 18 : token.decimals();
+  var i = 0;
+  var totalTokenBalance = new BigNumber(0);
+  console.log("RESULT:  # Account                                             EtherBalanceChange                          Token Name");
+  console.log("RESULT: -- ------------------------------------------ --------------------------- ------------------------------ ---------------------------");
+  accounts.forEach(function(e) {
+    var etherBalanceBaseBlock = eth.getBalance(e, baseBlock);
+    var etherBalance = web3.fromWei(eth.getBalance(e).minus(etherBalanceBaseBlock), "ether");
+    var tokenBalance = token == null ? new BigNumber(0) : token.balanceOf(e).shift(-decimals);
+    totalTokenBalance = totalTokenBalance.add(tokenBalance);
+    console.log("RESULT: " + pad2(i) + " " + e  + " " + pad(etherBalance) + " " + padToken(tokenBalance, decimals) + " " + accountNames[e]);
+    i++;
+  });
+  console.log("RESULT: -- ------------------------------------------ --------------------------- ------------------------------ ---------------------------");
+  console.log("RESULT:                                                                           " + padToken(totalTokenBalance, decimals) + " Total Token Balances");
+  console.log("RESULT: -- ------------------------------------------ --------------------------- ------------------------------ ---------------------------");
+  console.log("RESULT: ");
+}
+
+function pad2(s) {
+  var o = s.toFixed(0);
+  while (o.length < 2) {
+    o = " " + o;
+  }
+  return o;
+}
+
+function pad(s) {
+  var o = s.toFixed(18);
+  while (o.length < 27) {
+    o = " " + o;
+  }
+  return o;
+}
+
+function padToken(s, decimals) {
+  var o = s.toFixed(decimals);
+  var l = parseInt(decimals)+12;
+  while (o.length < l) {
+    o = " " + o;
+  }
+  return o;
+}
+
+
+// -----------------------------------------------------------------------------
+// Transaction status
+// -----------------------------------------------------------------------------
+function printTxData(name, txId) {
+  var tx = eth.getTransaction(txId);
+  var txReceipt = eth.getTransactionReceipt(txId);
+  var gasPrice = tx.gasPrice;
+  var gasCostETH = tx.gasPrice.mul(txReceipt.gasUsed).div(1e18);
+  var gasCostUSD = gasCostETH.mul(ethPriceUSD);
+  console.log("RESULT: " + name + " gas=" + tx.gas + " gasUsed=" + txReceipt.gasUsed + " costETH=" + gasCostETH +
+    " costUSD=" + gasCostUSD + " @ ETH/USD=" + ethPriceUSD + " gasPrice=" + gasPrice + " block=" + 
+    txReceipt.blockNumber + " txIx=" + tx.transactionIndex + " txId=" + txId);
+}
+
+function assertEtherBalance(account, expectedBalance) {
+  var etherBalance = web3.fromWei(eth.getBalance(account), "ether");
+  if (etherBalance == expectedBalance) {
+    console.log("RESULT: OK " + account + " has expected balance " + expectedBalance);
+  } else {
+    console.log("RESULT: FAILURE " + account + " has balance " + etherBalance + " <> expected " + expectedBalance);
+  }
+}
+
+function gasEqualsGasUsed(tx) {
+  var gas = eth.getTransaction(tx).gas;
+  var gasUsed = eth.getTransactionReceipt(tx).gasUsed;
+  return (gas == gasUsed);
+}
+
+function failIfGasEqualsGasUsed(tx, msg) {
+  var gas = eth.getTransaction(tx).gas;
+  var gasUsed = eth.getTransactionReceipt(tx).gasUsed;
+  if (gas == gasUsed) {
+    console.log("RESULT: FAIL " + msg);
+    return 0;
+  } else {
+    console.log("RESULT: PASS " + msg);
+    return 1;
+  }
+}
+
+function passIfGasEqualsGasUsed(tx, msg) {
+  var gas = eth.getTransaction(tx).gas;
+  var gasUsed = eth.getTransactionReceipt(tx).gasUsed;
+  if (gas == gasUsed) {
+    console.log("RESULT: PASS " + msg);
+    return 1;
+  } else {
+    console.log("RESULT: FAIL " + msg);
+    return 0;
+  }
+}
+
+function failIfGasEqualsGasUsedOrContractAddressNull(contractAddress, tx, msg) {
+  if (contractAddress == null) {
+    console.log("RESULT: FAIL " + msg);
+    return 0;
+  } else {
+    var gas = eth.getTransaction(tx).gas;
+    var gasUsed = eth.getTransactionReceipt(tx).gasUsed;
+    if (gas == gasUsed) {
+      console.log("RESULT: FAIL " + msg);
+      return 0;
+    } else {
+      console.log("RESULT: PASS " + msg);
+      return 1;
+    }
+  }
+}
+
+
+//-----------------------------------------------------------------------------
+// Token Contract
+//-----------------------------------------------------------------------------
+var tokenFromBlock = 0;
+function printTokenContractDetails() {
+  console.log("RESULT: tokenContractAddress=" + tokenContractAddress);
+  if (tokenContractAddress != null && tokenContractAbi != null) {
+    var contract = eth.contract(tokenContractAbi).at(tokenContractAddress);
+    var decimals = contract.decimals();
+    console.log("RESULT: token.totalSupply=" + contract.totalSupply().shift(-decimals));
+    console.log("RESULT: token.name=" + contract.name());
+    console.log("RESULT: token.symbol=" + contract.symbol());
+    console.log("RESULT: token.decimals=" + decimals);
+    /*
+    console.log("RESULT: token.totalEthReceivedInWei=" + contract.totalEthReceivedInWei().shift(-18));
+    console.log("RESULT: token.totalUsdReceived=" + contract.totalUsdReceived());
+    console.log("RESULT: token.version=" + contract.version());
+    console.log("RESULT: token.saleWalletAddress=" + contract.saleWalletAddress());
+    console.log("RESULT: token.snipCoinToEtherExchangeRate=" + contract.snipCoinToEtherExchangeRate());
+    console.log("RESULT: token.isSaleOpen=" + contract.isSaleOpen());
+    console.log("RESULT: token.transferable=" + contract.transferable());
+    console.log("RESULT: token.ethToUsdExchangeRate=" + contract.ethToUsdExchangeRate());
+    console.log("RESULT: token.contractOwner=" + contract.contractOwner());
+    console.log("RESULT: token.accountWithUpdatePermissions=" + contract.accountWithUpdatePermissions());
+    console.log("RESULT: token.DECIMALS_MULTIPLIER=" + contract.DECIMALS_MULTIPLIER());
+    console.log("RESULT: token.SALE_CAP_IN_USD=" + contract.SALE_CAP_IN_USD());
+    console.log("RESULT: token.MINIMUM_PURCHASE_IN_USD=" + contract.MINIMUM_PURCHASE_IN_USD());
+    console.log("RESULT: token.USD_PURCHASE_AMOUNT_REQUIRING_ID=" + contract.USD_PURCHASE_AMOUNT_REQUIRING_ID());
+    console.log("RESULT: token.getWeiToUsdExchangeRate=" + contract.getWeiToUsdExchangeRate());
+    */
+
+    var latestBlock = eth.blockNumber;
+    var i;
+
+    var approvalEvents = contract.Approval({}, { fromBlock: tokenFromBlock, toBlock: latestBlock });
+    i = 0;
+    approvalEvents.watch(function (error, result) {
+      console.log("RESULT: Approval " + i++ + " #" + result.blockNumber + " _owner=" + result.args._owner + " _spender=" + result.args._spender + " _value=" +
+        result.args._value.shift(-decimals));
+    });
+    approvalEvents.stopWatching();
+
+    var transferEvents = contract.Transfer({}, { fromBlock: tokenFromBlock, toBlock: latestBlock });
+    i = 0;
+    transferEvents.watch(function (error, result) {
+      console.log("RESULT: Transfer " + i++ + " #" + result.blockNumber + ": _from=" + result.args._from + " _to=" + result.args._to +
+        " _value=" + result.args._value.shift(-decimals));
+    });
+    transferEvents.stopWatching();
+
+    tokenFromBlock = latestBlock + 1;
+  }
+}
