b78e8daf361661f4dc99407af9d229e6401989bd
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2018-02-11 03:06:40 +1100

    Testing #5

---
+++ contracts/TestApproveAndCallFallback.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.18;
+
+// ----------------------------------------------------------------------------
+// ERC Token Standard #20 Interface
+// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
+// ----------------------------------------------------------------------------
+contract ERC20Interface {
+    function totalSupply() public view returns (uint);
+    function balanceOf(address tokenOwner) public view returns (uint balance);
+    function allowance(address tokenOwner, address spender) public view returns (uint remaining);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
+
+
+// ----------------------------------------------------------------------------
+// Contracts that can have tokens approved, and then a function execute
+// ----------------------------------------------------------------------------
+contract TestApproveAndCallFallBack {
+    event LogBytes(bytes data);
+
+    function receiveApproval(address from, uint256 tokens, address token, bytes data) public {
+        ERC20Interface(token).transferFrom(from, address(this), tokens);
+        LogBytes(data);
+    }
+}
